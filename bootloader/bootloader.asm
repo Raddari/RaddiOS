bd8d94e8f8380302c5cdd86dc7d9bb647666f188
Harvey <harveywyatt@hotmail.com>
2020-07-13 00:51:06 +0930

    Fix indentation

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -10,9 +10,9 @@ start: jmp boot
 msg db "Welcome to RaddiOS", 0ah, 0dh, 0h
 
 boot:
-    cli ; no interrupts
-    cld ; init
-    hlt ; halt
+        cli ; no interrupts
+        cld ; init
+        hlt ; halt
 
 ; We have to be 512 bytes, clear the rest of the bytes with 0
 times 510 - ($-$$) db 0
